31c7423080b6afb0b46bc39ccc9ab82026c25ba8
Diego Marroquin <326179697+diegomarroquinDev@users.noreply.github.com>
2026-09-08 14:45:23 -0600

    Agregar proyecto MVC con Razor

---
+++ GestionTarjetaCredito.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionTarjetaCredito.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
